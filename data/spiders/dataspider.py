19c5420f2137baea3ad663aded503946f62bd12a
simba999 <oliverking8985@yahoo.com>
2018-04-09 17:17:01 -0600

    update

--- data/spiders/dataspider.py
+++ data/spiders/dataspider.py
@@ -1,17 +1,23 @@
 from __future__ import unicode_literals
 import scrapy
 import json
-import os
+import os, sys
 from scrapy.spiders import Spider
 from scrapy.http import FormRequest
 from scrapy.http import Request
 from data.items import DataItem
 from lxml import etree
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from lxml import html
 import time
 import datetime
 import random
+import Tkinter as tk
+import tkMessageBox
 import pdb
 
 
@@ -21,9 +27,12 @@ class DataspiderSpider(scrapy.Spider):
     start_urls = ['https://ttdsevaonline.chromedriver']
 
     def __init__(self):
-        self.driver = webdriver.Chrome("./chromedriver")
+        _chrome_options = Options()
+        _chrome_options.add_argument('disable-infobars')
+        self.driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=_chrome_options)
         self.driver.set_window_size(1850, 1000)
-        with open('input.txt', 'r') as data_file:
+        filename = 'input.txt'
+        with open(filename, 'r') as data_file:
             self.input_list = json.load(data_file)
         script_dir = os.path.dirname(__file__)
 
@@ -32,6 +41,9 @@ class DataspiderSpider(scrapy.Spider):
         yield scrapy.Request(url=init_url, callback=self.body)
 
     def body(self, response):
+        root = tk.Tk()
+        root.withdraw()
+
         for item in self.input_list:
             username = item['email']
             password = item['password']
@@ -40,45 +52,53 @@ class DataspiderSpider(scrapy.Spider):
             number_of_additional_laddus = str(int(number_of_user) * 2)
 
             self.driver.get("https://ttdsevaonline.com/#/login")
-            time.sleep(3)
+            ele = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, 'login_sub')))
+
             self.driver.find_element_by_xpath('//input[contains(@class, "u_name")]').send_keys(username)
             self.driver.find_element_by_xpath('//input[contains(@class, "u_pass")]').send_keys(password)
-            self.driver.find_element_by_xpath("//button[@id='login_sub']").click()
+            ele.click()
 
-            time.sleep(5)
-            self.driver.find_element_by_xpath('//div[@class="menu"]//span[@class="Services"]').click()
-            time.sleep(5)
-            self.driver.find_element_by_xpath('//ul[@class="nav"]//li//a[contains(@class, "eSpecial")]').click()
-            time.sleep(5)
+            ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="menu"]//span[@class="Services"]')))
+            # pdb.set_trace(2)
+            ele.click()
+            # self.driver.find_element_by_xpath('//div[@class="menu"]//span[@class="Services"]').click()
+            
+            # time.sleep(5)
+            ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//ul[@class="nav"]//li//a[contains(@class, "eSpecial")]')))
+            ele.click()
+            # time.sleep(5)
+            str1 = '//span[contains(@class, "' + today_date + '")]'
+            ele = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, str1)))
             source = self.driver.page_source.encode("utf8")
             tree = etree.HTML(source)
 
             current_span = tree.xpath('//span[contains(@class, "' + today_date + '")]')
-
             status = ''
 
             if len(current_span) == 0:
-                print('no such date')
+                tkMessageBox.showwarning('Title','Date is invalid')
                 break
             else:
                 status = current_span[0].xpath('./@class')[0]
 
                 if 'booked' in status:
-                    print('already booked')
+                    tkMessageBox.showwarning('Title', 'Already Booked!')
                     break
                 elif 'prevDay' in status or 'quotaNotReleased' in status:
-                    print ('Not available')
+                    tkMessageBox.showwarning('Title','Not available')
                     break
                 else:
                     _path = etree.ElementTree(tree).getpath(current_span[0])
                     self.driver.find_element_by_xpath(_path).click()
-                    time.sleep(6)
-                    
-                    self.driver.find_element_by_xpath('(//div[@class="cal_inrcnt"])[1]').click()
+                    time.sleep(2)
+                    ele = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '(//div[@class="cal_inrcnt"])[1]')))
+                    ele.click()
 
                     # click number of people
-                    self.driver.find_element_by_xpath('//select[@id="person"]').click()
-                    time.sleep(1)
+                    ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//select[@id="person"]')))
+                    ele.click()
+                    # self.driver.find_element_by_xpath('//select[@id="person"]').click()
+                    # time.sleep(1)
                     source = self.driver.page_source.encode("utf8")
                     tree = etree.HTML(source)
 
@@ -91,8 +111,11 @@ class DataspiderSpider(scrapy.Spider):
                             self.driver.find_element_by_xpath(_path).click()
                             break
 
-                    self.driver.find_element_by_xpath('//select[@id="person"]').click()
-                    time.sleep(1)
+                    ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//select[@id="person"]')))
+                    ele.click()
+                    # self.driver.find_element_by_xpath('//select[@id="person"]').click()
+                    # time.sleep(1)
+                    ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//select[@id="addladdus"]//option')))
                     source = self.driver.page_source.encode("utf8")
                     tree = etree.HTML(source)
                     additional_laddus_list = tree.xpath('//select[@id="addladdus"]//option')
@@ -106,14 +129,17 @@ class DataspiderSpider(scrapy.Spider):
 
                     self.driver.find_element_by_xpath('//input[@id="booking_others"]').click()
                     self.driver.find_element_by_xpath('//button[@id="sedavailability_contiune"]').click()
-                    time.sleep(8)
                     
+                    ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//form[contains(@id, "othersForm")]')))
                     source = self.driver.page_source.encode("utf8")
                     tree = etree.HTML(source)
                     form_list = tree.xpath('//form[contains(@id, "othersForm")]')
 
                     num = 0
+                    print form_list
+
                     for form in form_list:
+                        # pdb.set_trace()
                         # name input
                         selector = 'fName' + str(num)
                         name_element = tree.xpath('//input[contains(@name, "' + selector + '")]')[0]
@@ -127,31 +153,41 @@ class DataspiderSpider(scrapy.Spider):
                         
                         # select gender
                         selector = 'gender' + str(num)
-                        self.driver.find_element_by_xpath('//select[contains(@name, "' + selector + '")]').click()
-                        time.sleep(1)
+                        ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//select[contains(@name, "' + selector + '")]')))
+                        ele.click()
+                        # time.sleep(1)
                         option_list = tree.xpath('//select[contains(@name, "' + selector + '")]//option')
 
                         for option in option_list:
-                            gender = option.xpath('./text()')[0]
-                            if item['people'][num]['gender'] in gender:
-                                _path = etree.ElementTree(tree).getpath(option)
-                                self.driver.find_element_by_xpath(_path).click()
-                                break
+                            try:
+                                gender = option.xpath('./text()')[0]
+                                if item['people'][num]['gender'] in gender:
+                                    _path = etree.ElementTree(tree).getpath(option)
+                                    self.driver.find_element_by_xpath(_path).click()
+                                    break
+                            except:
+                                pass
+                            
 
                         # select id category
                         selector = 'proofS' + str(num)
-                        self.driver.find_element_by_xpath('//select[contains(@name, "' + selector + '")]').click()
-                        time.sleep(1)
+                        ele = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//select[contains(@name, "' + selector + '")]')))
+                        ele.click()
+                        # self.driver.find_element_by_xpath('//select[contains(@name, "' + selector + '")]').click()
+                        # time.sleep(1)
                         source = self.driver.page_source.encode("utf8")
                         tree = etree.HTML(source)
                         option_list = tree.xpath('//select[contains(@name, "' + selector + '")]//option')
 
                         for option in option_list:
-                            gender = option.xpath('./text()')[0]
-                            if item['people'][num]['id'] in gender:
-                                _path = etree.ElementTree(tree).getpath(option)
-                                self.driver.find_element_by_xpath(_path).click()
-                                break
+                            try:
+                                gender = option.xpath('./text()')[0]
+                                if item['people'][num]['id'] in gender:
+                                    _path = etree.ElementTree(tree).getpath(option)
+                                    self.driver.find_element_by_xpath(_path).click()
+                                    break
+                            except:
+                                pass
 
                         num = num + 1
 
@@ -165,10 +201,9 @@ class DataspiderSpider(scrapy.Spider):
                         self.driver.find_element_by_xpath(_path).send_keys(item['people'][num]['id_number'])
                         num = num + 1
 
-                    pdb.set_trace()
-                    self.driver.find_element_by_xpath('//button[@id="sedpiligrim_continue"]').click()
-                    time.sleep(5)
-                    if 'https://ttdsevaonline.com/#/sedPiligrim' is self.driver.current_url:
-                        print ('Error in continue')
-                    else:
-                        pass
+                    time.sleep(2)
+                    if 'https://ttdsevaonline.com/#/sedPiligrim' in self.driver.current_url:
+                        tkMessageBox.showwarning('Title','INPUT ERROR')
+                        self.driver.find_element_by_xpath('//button[@id="sedpiligrim_continue"]').click()
+                        # time.sleep(2)
+
